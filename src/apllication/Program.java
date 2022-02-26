2dac475cfb96c04f3fd24d6d01cd193e7ef73140
Leandro Abahit Benitti <leandroabenitti@hotmail.com>
2022-02-26 15:33:09 -0300

    Progeto Xadrez

---
+++ src/apllication/Program.java
@@ -0,0 +1,13 @@
+package apllication;
+
+import chess.ChessMatch;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		ChessMatch chessMatch = new ChessMatch();
+		UI.printBoard(chessMatch.getPieces());
+	}
+
+}
